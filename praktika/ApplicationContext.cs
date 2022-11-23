63fd3f0e1615175418e98c1caeac653be030f3ac
requiem2ks <105455039+requiem2ks@users.noreply.github.com>
2022-11-24 00:28:10 +0300

    Add files via upload

---
+++ praktika/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.IO;
+
+
+namespace praktika1
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("jscfg.json");
+            var config = builder.Build();
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
+   
+
+            
