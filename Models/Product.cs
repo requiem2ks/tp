c06c220c52441cb08c879e910c42f1e181b36ace
requiem2ks <105455039+requiem2ks@users.noreply.github.com>
2022-11-24 03:56:29 +0300

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicShop
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? Title { get; set; }
+        public string? Price { get; set; }
+        public bool? TheNeedForCustomization { get; set; }
+        public int? ProviderOfProducts { get; set; }
+    }
+}
